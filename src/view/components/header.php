f1075ed043dfbd6609c261f4f1f18ba038fe5a16
annaLucian <garielapoves01@gmail.com>
2024-02-15 15:26:11 +0100

    feat: :tada: installation of the correct dependencies

---
+++ src/view/components/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../../resources/css/base.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
