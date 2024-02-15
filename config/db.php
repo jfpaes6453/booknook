0067dce3a76185ccb5d1eb18bb6eba0bfa54e109
Gabi <hgpc314@gmail.com>
2024-02-15 15:05:09 +0100

    build: :rocket: tailwind

---
+++ config/db.php
@@ -0,0 +1,62 @@
+<?php
+
+class Database
+{
+    private $host;
+    private $db;
+    private $user;
+    private $password;
+    private $charset;
+    private $pdo;
+
+    public function __construct()
+    {
+        // Load .env file
+        $dotenv = parse_ini_file('.env');
+        
+        // Set database configuration
+        $this->host = $dotenv['DB_HOST'];
+        $this->db = $dotenv['DB_NAME'];
+        $this->user = $dotenv['DB_USER'];
+        $this->password = $dotenv['DB_PASSWORD'];
+        $this->charset = 'utf8mb4';
+        
+        // Initialize PDO
+        $this->connect();
+    }
+
+    private function connect()
+    {
+        $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $this->user, $this->password, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public function getPDO()
+    {
+        return $this->pdo;
+    }
+
+    public function testConnection()
+    {
+        try {
+            $statement = $this->pdo->query("SELECT 1");
+            $result = $statement->fetchAll();
+            return $result ? "Connection is successful." : "Connection test failed.";
+        } catch (\PDOException $e) {
+            return "Error on connection test: " . $e->getMessage();
+        }
+    }
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require './config/db.php';
+
+$db = new Database();
+echo $db->testConnection();
+
+?>
+ 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link href="./src/css/styles.css" rel="stylesheet"> -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+<h1 class="bg-black">hola</h1>
+    
+</body>
+</html>
