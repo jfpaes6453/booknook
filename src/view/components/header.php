4b3b0384ae0cb0bc7434a1a6d102077539e39029
Gabi <hgpc314@gmail.com>
2024-02-17 00:36:26 +0100

    refactor: :sparkles: header refactor

--- src/view/components/header.php
+++ src/view/components/header.php
@@ -1,31 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../../resources/css/base.css" rel="stylesheet">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Online BookNook</title>
-
-</head>
-
-<body>
-
     <header class="bg-blue-200 text-white py-4 px-6 flex justify-between items-center">
-        <div>
-            <img src="http://localhost/booknook/src/resources/img/logo_1.png" alt="Logo">
-        </div>
-        <div class="flex items-center space-x-32 justify-end flex-grow">
+        <img src="src\resources\img\logo_1.png" alt="Logo">
+        <nav class="flex items-center space-x-32 justify-end flex-grow">
             <a href="/ruta/a/home" class="text-center text-zinc-800 text-[26px] font-semibold font-['Nunito Sans']" style="position: relative;">HOME<span style="position: absolute; bottom: 0; left: 0; right: 0; height: 5px; background-color: #FF621E;"></span></a>
-            <a href="/ruta/a/login" class="text-center text-zinc-800 text-[26px] font-semibold font-['Nunito Sans']">LOGIN</a>
-        </div>
-        <div>
-            <img src="http://localhost/booknook/src/resources/img/icon_user.png" alt="User Icon">
-        </div>
-
+            <div class="flex">
+                <a href="/ruta/a/login" class="text-center text-zinc-800 text-[26px] font-semibold font-['Nunito Sans']">LOGIN</a>
+                <img src="src\resources\img\icon_user.png" alt="User Icon">
+            </div>
+        </nav>
     </header>
-
-</body>
-
-</html>
