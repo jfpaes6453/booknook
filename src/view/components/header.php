d21649650f02a7093b070a67af7d099f828104c2
StephaniaGs <150010246+StephaniaGs@users.noreply.github.com>
2024-02-16 12:12:39 +0100

    feat: Added assets/images folder

--- src/view/components/header.php
+++ src/view/components/header.php
@@ -6,8 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../../resources/css/base.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>Online BookNook</title>
+
 </head>
+<body>
+    <header class="bg-white text-black py-4 px-6 flex justify-between items-center border-b-4 border-indigo-500">
+        <div class="flex items-center space-x-4">
+            <a href="/ruta/a/tu/pagina">
+                <img src="/ruta/a/tu/logo.png" alt="Logo">
+            </a>
+            <span>HOME</span>
+            <span>ADMIN/DESKTOP</span>
+        </div>
+        <div>
+            <h1 class="text-lg font-bold">Online BookNook</h1>
+        </div>
+        <div class="flex items-center space-x-4">
+            <span></span>
+            <span>INSUJPT</span>
+            <span>LOREM</span>
+            <span class="icon-person"></span>
+        </div>
+    </header>
+</body>
+</html>
 
 <body>
 
