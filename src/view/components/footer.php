8a6db65d0be9d46f62bec18d9a75782914cdc600
Jasz-17 <jaszapata1983@gmail.com>
2024-02-18 16:50:40 +0100

    style: :sparkles: footer-create

--- src/view/components/footer.php
+++ src/view/components/footer.php
@@ -1,14 +1,16 @@
-<footer class="footer bg-gray-800 text-white p-4">
-    <div class="footer-content flex justify-between items-center">
-        <div class="footer-logo">
-            <img src="public/img/tu_logo.png" alt="Logo de la biblioteca" class="w-16 h-16">
-        </div>
-        <div class="footer-icons">
-            <i class="fab fa-facebook text-xl mx-2"></i>
-            <i class="fab fa-twitter text-xl mx-2"></i>
-        </div>
-    </div>
-    <div class="footer-text mt-4">
-        <p>&copy; <?php echo date("Y"); ?> Biblioteca Moderna - Todos los derechos reservados</p>
+<footer class="fixed bottom-0 left-0 w-full bg-cover bg-center" style="background-image: url('src/resources/img/img-footer.svg'); background-size: cover; background-repeat: no-repeat; height: 100px;">
+    <div class="footer-icons flex justify-center space-x-4 items-center h-full">
+    <a href="#" class="mx-1">
+            <img src="src\resources\img\icons\icon-facebook.svg" alt="Facebook" class="w-6 h-6">
+        </a>
+        <a href="#" class="mx-1">
+            <img src="src/resources/img/icons/icon-twitter.svg" alt="Twitter" class="w-6 h-6">
+        </a>
+        <a href="#" class="mx-1">
+            <img src="src/resources/img/icons/icon-youtube.svg" alt="YouTube" class="w-6 h-6">
+        </a>
+        <a href="#" class="mx-1">
+            <img src="src/resources/img/icons/icon-instagram.svg" alt="Instagram" class="w-6 h-6">
+        </a>
     </div>
 </footer>
